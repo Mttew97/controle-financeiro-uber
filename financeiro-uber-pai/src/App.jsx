3b50d99f173fbe868c1a178727844b09127a1c62
Matheus Santana <matheuscloud2005@gmail.com>
2026-08-26 17:48:22 -0300

    adcioa formulario de gastos

--- financeiro-uber-pai/src/App.jsx
+++ financeiro-uber-pai/src/App.jsx
@@ -10,6 +10,9 @@ function App() {
     const [data, setData] = useState('')
     const [valorGanho, setValorGanho] = useState('')
     const [horasTrabalhadas, setHorasTrabalhadas] = useState('')
+    const [descricaoGasto, setdescricaoGasto] = useState('')
+    const [valorGasto, setvalorGasto] = useState('')
+    const [dataGasto, setdataGasto] = useState('')
     
     async function handleSubmit(e) {
     e.preventDefault()
@@ -29,6 +32,32 @@ function App() {
           console.error('Erro ao salvar:', error)
       } else{
         console.log('Salvo com sucesso:', resultado)
+        setCorridas('')
+        setData('')
+        setValorGanho('')
+        setHorasTrabalhadas('')
+      }}
+
+       async function handleSubmitGasto(e) {
+        e.preventDefault()
+
+    const {data: resultado, error} = await supabase
+      .from('gastos')
+      .insert([
+        {
+          data:dataGasto,
+          valor: parseFloat(valorGasto),
+          descricao: descricaoGasto,
+        }
+      ])
+      
+      if(error) {
+          console.error('Erro ao salvar:', error)
+      } else{
+        console.log('Salvo com sucesso:', resultado)
+        setdataGasto('')
+        setvalorGasto('')
+        setdescricaoGasto('')
       }}
 
 return (
@@ -64,6 +93,27 @@ return (
         onChange={(e) => setHorasTrabalhadas(e.target.value)}
       />
       <button type="submit">Salvar</button>
+      </form>
+      <form onSubmit={handleSubmitGasto}>
+        <label htmlFor='descricaoGasto'>Descreva seu gasto</label>
+        <input
+        id='descricaoGasto'
+        type='text'
+        value={descricaoGasto}
+        onChange={(e) => setdescricaoGasto(e.target.value)} />
+        <label htmlFor='valorGasto'>Qual valor foi gasto</label>
+        <input
+        id='valorGasto'
+        type='number'
+        value={valorGasto}
+        onChange={(e) => setvalorGasto(e.target.value)} />
+        <label htmlFor='dataGasto'>Qual a data do gasto</label>
+        <input
+        id='dataGasto'
+        type='date'
+        value={dataGasto}
+        onChange={(e) => setdataGasto(e.target.value)} />
+        <button type="submit">Salvar Gasto</button>
       </form>
     </>
   )
